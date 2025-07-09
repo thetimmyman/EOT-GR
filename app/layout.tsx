9a78e37c4bedf0aa46f79c18d29dd66df6fea11f
thetimmyman <tim.defreest@gmail.com>
2025-07-09 21:26:37 +0000

    adding vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: 'Guild Raid Analytics',
